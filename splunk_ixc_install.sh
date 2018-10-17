24f04f54003a293a01ed0fd52c26b3c77184774d
Philippe Tang <ptang@splunk.com>
2018-10-17 10:15:50 -0400

    single-site

--- splunk_ixc_install.sh
+++ splunk_ixc_install.sh
@@ -5,4 +5,4 @@ SPLUNK_HOME=/opt/splunk
 APPS=$SPLUNK_HOME/etc/apps
 
 cp -r $CONFIG_SPLK_PATH/hawkEye_cluster_idx_base $APPS/
-cp -r $CONFIG_SPLK_PATH/hawkEye_cluster_site29_idx_base $APPS/
+#cp -r $CONFIG_SPLK_PATH/hawkEye_cluster_site29_idx_base $APPS/
